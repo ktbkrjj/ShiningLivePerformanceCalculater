17b0bf6c715adf507a4e01a2f20fa6590be288be
n.kenju <n.kenju.co.@gmail.com>
2017-10-14 23:17:58 +0900

    [fix] listView が 6 より少ないときに落ちないようにした

--- Sources/app/src/main/java/com/example/kenju/shiningliveperformancecalculater/Calucurater.kt
+++ Sources/app/src/main/java/com/example/kenju/shiningliveperformancecalculater/Calucurater.kt
@@ -4,7 +4,9 @@ class Calucurater(val bromides_: MutableList<BromideData>) {
     fun getTotalValue() : Int {
         var total = 0
         for(index in 1..6) {
-            total += bromides_[index].star + bromides_[index].shine + bromides_[index].dream
+            if (bromides_.count() > index) {
+                total += bromides_[index].star + bromides_[index].shine + bromides_[index].dream
+            }
         }
         return total
     }
