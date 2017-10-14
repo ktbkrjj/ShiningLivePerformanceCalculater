b36145259e6d4bc9dc24ffcbbc97b5ba2a8669d7
n.kenju <n.kenju.co.@gmail.com>
2017-10-14 21:42:37 +0900

    [refactor] BromideAdapter を別ファイルに移動した

--- Sources/app/src/main/java/com/example/kenju/shiningliveperformancecalculater/MainActivity.kt
+++ Sources/app/src/main/java/com/example/kenju/shiningliveperformancecalculater/MainActivity.kt
@@ -41,40 +41,6 @@ class MainActivity : AppCompatActivity() {
         mListView?.adapter = BromideAdapter(this, preset)
     }
 
-    data class ViewHolder(val nameTextView: TextView, val attributeImageView: ImageView, val starTextView: TextView, val shineTextView: TextView, val dreamTextView: TextView)
-
-    class BromideAdapter(context: Context, items: List<BromideData>) : ArrayAdapter<BromideData>(context, 0, items) {
-        val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            var view = convertView
-            var holder: ViewHolder
-
-            if (view == null) {
-                view = layoutInflater.inflate(R.layout.listview_item, parent, false)
-                holder = ViewHolder(
-                        view?.findViewById<TextView>(R.id.itemName) as TextView,
-                        view?.findViewById<ImageView>(R.id.itemImageAttribute) as ImageView,
-                        view?.findViewById<TextView>(R.id.itemStarValue) as TextView,
-                        view?.findViewById<TextView>(R.id.itemShineValue) as TextView,
-                        view?.findViewById<TextView>(R.id.itemDreamValue) as TextView
-                )
-                view?.tag = holder
-            } else {
-                holder = view?.tag as ViewHolder
-            }
-
-            val item = getItem(position) as BromideData
-            holder.nameTextView.text = item.name
-            holder.attributeImageView.setImageBitmap(BitmapFactory.decodeResource(context.resources, item.imageId))
-            holder.starTextView.text = item.star.toString()
-            holder.shineTextView.text = item.shine.toString()
-            holder.dreamTextView.text = item.dream.toString()
-
-            return view!!
-        }
-    }
-
     internal inner class DragListener : SortableListView.SimpleDragListener() {
         var mDraggingPosition = -1
 
